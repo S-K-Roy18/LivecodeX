197b1975f83d0756f9d5b1d4c2ff5a0c4e643895
S-K-Roy18 <suryarahulroy321@gmail.com>
2026-08-01 12:54:37 +0530

    session routes and  added

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,5 @@
 import { StreamChat } from "stream-chat";
-// import { StreamClient } from "@stream-io/node-sdk";
+import { StreamClient } from "@stream-io/node-sdk";
 import { ENV } from "./env.js";
 
 const apiKey = ENV.STREAM_API_KEY;
@@ -10,7 +10,7 @@ if (!apiKey || !apiSecret) {
 }
 
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
-// export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
 export const upsertStreamUser = async (userData) => {
   try {
@@ -29,5 +29,3 @@ export const deleteStreamUser = async (userId) => {
     console.error("Error deleting the Stream user:", error);
   }
 };
-
-// todo: add another method to generateToken
